79f8420232db725261ad0cf2da45009102ae1200
钟达烽 <zhongdafeng@qfun.com>
2018-10-09 16:48:09 +0800

    Update Demo

---
+++ app/src/main/java/com/ruffian/library/demo/GradientActivity.java
@@ -0,0 +1,20 @@
+package com.ruffian.library.demo;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Gradient相关演示
+ *
+ * @author ZhongDaFeng
+ */
+public class GradientActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gradient);
+    }
+}
+
--- app/src/main/java/com/ruffian/library/demo/MainActivity.java
+++ app/src/main/java/com/ruffian/library/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.ruffian.library.demo;
 
 import android.content.Intent;
+import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -16,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.et).setOnClickListener(this);
         findViewById(R.id.view_group).setOnClickListener(this);
         findViewById(R.id.view).setOnClickListener(this);
+        findViewById(R.id.gradient).setOnClickListener(this);
     }
 
 
@@ -37,6 +39,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.view:
                 intent = new Intent(this, RViewActivity.class);
+                break;
+            case R.id.gradient:
+                intent = new Intent(this, GradientActivity.class);
                 break;
         }
         if (intent != null) {
