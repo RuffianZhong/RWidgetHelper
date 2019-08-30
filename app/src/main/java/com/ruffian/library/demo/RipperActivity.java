3154e26bb01bac454fb15d21eec7c8be27e8051c
钟达烽 <zhongdafeng@qfun.com>
2019-08-30 16:58:56 +0800

    Update Demo

--- app/src/main/java/com/ruffian/library/demo/RipperActivity.java
+++ app/src/main/java/com/ruffian/library/demo/RipperActivity.java
@@ -3,6 +3,10 @@ package com.ruffian.library.demo;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.ruffian.library.widget.RButton;
+import com.ruffian.library.widget.RTextView;
 
 
 /**
@@ -11,10 +15,22 @@ import android.support.v7.app.AppCompatActivity;
  * @author ZhongDaFeng
  */
 public class RipperActivity extends AppCompatActivity {
+    private RTextView tvTag;
+    private RButton btnUpdate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ripper);
+        tvTag = (RTextView) findViewById(R.id.tv_tag);
+        btnUpdate = (RButton) findViewById(R.id.tv_update);
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean enabled = tvTag.isEnabled();
+                tvTag.setEnabled(!enabled);
+                btnUpdate.setText(String.valueOf(!enabled));
+            }
+        });
     }
 }
