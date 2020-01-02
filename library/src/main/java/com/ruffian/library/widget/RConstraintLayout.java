f65c356d14aff4d043cdc71f9ff607d062d854e4
RuffianZhong <632835821@qq.com>
2020-01-02 19:45:45 +0800

    Support ConstraintLayout

---
+++ library/src/main/java/com/ruffian/library/widget/RConstraintLayout.java
@@ -0,0 +1,36 @@
+package com.ruffian.library.widget;
+
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
+import android.util.AttributeSet;
+
+import com.ruffian.library.widget.helper.RBaseHelper;
+import com.ruffian.library.widget.iface.RHelper;
+
+/**
+ * RConstraintLayout
+ *
+ * @author ZhongDaFeng
+ */
+public class RConstraintLayout extends ConstraintLayout implements RHelper<RBaseHelper> {
+
+    private RBaseHelper mHelper;
+
+    public RConstraintLayout(Context context) {
+        this(context, null);
+    }
+
+    public RConstraintLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RConstraintLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        mHelper = new RBaseHelper(context, this, attrs);
+    }
+
+    @Override
+    public RBaseHelper getHelper() {
+        return mHelper;
+    }
+}
