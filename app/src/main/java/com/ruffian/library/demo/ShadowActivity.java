ca9f4b5492a183b8040fac1e3c630a6db2c4dfaa
RuffianZhong <632835821@qq.com>
2020-01-10 10:32:55 +0800

    rollback demo support androidx

--- app/src/main/java/com/ruffian/library/demo/ShadowActivity.java
+++ app/src/main/java/com/ruffian/library/demo/ShadowActivity.java
@@ -1,10 +1,10 @@
 package com.ruffian.library.demo;
 
 import android.os.Bundle;
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
