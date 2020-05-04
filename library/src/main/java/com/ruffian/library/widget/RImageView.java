4c3e72815df4118386bbfb490a874a828ae93049
RuffianZhong <632835821@qq.com>
2020-05-04 19:17:31 +0800

    Fix bug

--- library/src/main/java/com/ruffian/library/widget/RImageView.java
+++ library/src/main/java/com/ruffian/library/widget/RImageView.java
@@ -11,11 +11,11 @@ import android.graphics.drawable.LayerDrawable;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
-import com.ruffian.library.widget.rounded.RoundDrawable;
-
 import androidx.annotation.ColorInt;
 import androidx.annotation.DrawableRes;
 import androidx.annotation.Nullable;
+
+import com.ruffian.library.widget.rounded.RoundDrawable;
 
 /**
  * RImageView
@@ -153,8 +153,17 @@ public class RImageView extends ImageView {
      */
     private void drawEmptyBitmap() {
         if (mDrawable == null) {//未设置Bitmap
-            Bitmap empty = Bitmap.createBitmap(getMeasuredWidth(), getMeasuredHeight(), Bitmap.Config.ALPHA_8);
-            setImageBitmap(empty);
+            int width = getMeasuredWidth();
+            int height = getMeasuredHeight();
+            if (width > 0 && height > 0) {
+                Drawable background = getBackground();
+                if (background != null) {
+                    background.setBounds(0, 0, width, height);
+                    setImageDrawable(background);
+                } else {
+                    setImageBitmap(Bitmap.createBitmap(width, height, Bitmap.Config.ALPHA_8));
+                }
+            }
         }
     }
 
